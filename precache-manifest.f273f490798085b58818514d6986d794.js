c2f043b8faf63d94119ff01b6902b5e10e318305
felipejoribeiro <feliperibeiro.ufu@gmail.com>
2020-11-08 23:57:25 -0300

    Updates

---
+++ precache-manifest.f273f490798085b58818514d6986d794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a222f0977f1c353a1eb8e2dfa0a398",
+    "url": "/React-To-My-Books/index.html"
+  },
+  {
+    "revision": "bab74671297127469981",
+    "url": "/React-To-My-Books/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cc790bf6d842a925ec71",
+    "url": "/React-To-My-Books/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "bab74671297127469981",
+    "url": "/React-To-My-Books/static/js/2.d0237aab.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/React-To-My-Books/static/js/2.d0237aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc790bf6d842a925ec71",
+    "url": "/React-To-My-Books/static/js/main.b5cd27a9.chunk.js"
+  },
+  {
+    "revision": "618df318fd090d6e6594",
+    "url": "/React-To-My-Books/static/js/runtime-main.e32be6a5.js"
+  }
+]);
